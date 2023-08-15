318ea2f3005d2992b0d7cae01dcb8ce4bfc7ea30
jmznt4345 <jameszazalll15@gmail.com>
2023-08-16 04:58:43 +0700

    lab 3 not success

---
+++ lab3-angular-app/src/app/components/parent/parent.component.spec.ts
@@ -0,0 +1,17 @@
+import { OnInit } from '@angular/core';
+
+export class ParentComponent implements OnInit {
+
+  parentData!: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.parentData = 0;
+  }
+
+  onClick2Child(){
+    this.parentData += 1 ;
+  }
+
+}
